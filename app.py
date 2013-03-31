08c899111c6eef01f23ddab30822fac51993b7b3
Jason Wu <jaswu@eden.rutgers.edu>
2013-03-30 22:16:27 -0400

    fixing port binding

--- app.py
+++ app.py
@@ -13,5 +13,7 @@ def hackru():
 def sample():
 	return render_template('sample.html', my_variable='Variable substitution worked')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
